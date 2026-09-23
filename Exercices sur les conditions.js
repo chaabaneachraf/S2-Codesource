52ec8a8c9111cd619a2b9a1aa746839b92cee2de
chaabaneachraf <chaabanachraf0@gmail.com>
2026-09-23 16:15:48 +0100

    Update Exercices sur les conditions.js

--- Exercices sur les conditions.js
+++ Exercices sur les conditions.js
@@ -1,3 +1,4 @@
+//Exercice 1
 let note = 12;
 if ( note >=16) {
     console.log("tres bien")
@@ -5,4 +6,20 @@ if ( note >=16) {
     console.log("Valide")
 } else(
     console.log( "No valide")
-)
+) // valide
+//Exercice 2
+let temparature=28;
+if(temprature >=25) {
+    console.log("chaud")
+}else if(temparature<10 ) {
+    console.log("froid")
+}else(
+    console.log("doux")
+    ) //chaud
+//Exercice 3
+let note = 14; 
+let presence= 80;
+if ( note>=14 && presence >=80) {
+    console.log("valide")
+} else (
+    console.log("no valide") )  //valide
